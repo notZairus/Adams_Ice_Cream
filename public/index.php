b7ab5b36b9dc6966aa87b06c29966bee2117e3cc
notZairus <notzairus@gmail.com>
2024-10-03 21:36:32 +0700

    upload public dir

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<?php
+
+require('../functions.php');
+
+spl_autoload_register(function($class) {
+  if (file_exists(base_path("{$class}.php"))) {
+    return require (base_path("{$class}.php"));
+  }
+});
+
+
+$router = new Router();
+
+require(base_path('routes.php'));
+
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+$router->route($uri);
