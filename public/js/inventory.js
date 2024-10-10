21f34634266f4e31e94d508cb2cb1bf002be2691
notZairus <notzairus@gmail.com>
2024-10-10 07:05:39 +0700

    finished the add stock feature

--- public/js/inventory.js
+++ public/js/inventory.js
@@ -1,23 +1,49 @@
 
+//NEW INGREDIENT
+//NEW INGREDIENT
+//NEW INGREDIENT
+
 let show_new_ingredient_modal = document.getElementById('show_new_ingredient_modal')
 let close_new_ingredient_modal = document.getElementById('close_new_ingredient_modal')
 let new_ingredient_modal = document.getElementById('new_ingredient_modal')
 
 
-show_new_ingredient_modal.addEventListener('click', () => {
+show_new_ingredient_modal.onclick = () => {
   new_ingredient_modal.showModal();
-});
+};
 
-close_new_ingredient_modal.addEventListener('click', () => {
+close_new_ingredient_modal.onclick = () => {
   new_ingredient_modal.close();
   document.getElementById('ingredient_name').value = "";
   document.getElementById('ingredient_price').value = "";
   document.getElementById('ingredient_unit').value = "";
-});
+};
 
-
-
-let new_ingredient_form = document.querySelector('.new-ingredient-form');
-new_ingredient_form.onsubmit = (event) => {
+document.querySelector('.new-ingredient-form').onsubmit = (event) => {
   alert('Ingredient Added Successfully!');
+}
+
+
+//ADD STOCK
+//ADD STOCK
+//ADD STOCK
+
+let show_add_stock_modal = document.getElementById('show_add_stock_modal')
+let close_add_stock_modal = document.getElementById('close_add_stock_modal')
+let add_stock_modal = document.getElementById('add_stock_modal')
+
+
+show_add_stock_modal.onclick = () => {
+  add_stock_modal.showModal();
+}
+
+close_add_stock_modal.onclick = () => {
+  add_stock_modal.close();
+  document.getElementById('ingredient_id').value = "";
+  document.getElementById('new_stocks').value = "";
+  document.getElementById('ingredient_unit').value = "";
+};
+
+document.querySelector('.add-stock-form').onsubmit = (event) => {
+  alert("Stock Sucessfully Added!");
 }
